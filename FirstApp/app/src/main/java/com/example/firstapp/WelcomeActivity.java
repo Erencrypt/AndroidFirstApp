4cd06ec3ab8cce88ad348c191156c9a22578e054
ErenCanUtku <erencan.utku52@gmail.com>
2020-10-22 17:19:10 +0300

    initial commit
    this is my first app

---
+++ FirstApp/app/src/main/java/com/example/firstapp/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView tvName;
+    private String  strName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        tvName=(TextView) findViewById(R.id.tvName);
+        Bundle bundle =getIntent().getExtras();
+        strName=bundle.getString("NAME");
+        tvName.setText(strName);
+
+    }
+}
